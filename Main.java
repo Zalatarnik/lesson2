c184260eee4641b998ce05159f32050d2f86fa3d
Zalatarnik <Zalatarnik@gmail.com>
2023-10-25 18:45:15 +0300

    Conflict in Main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world.");
+        System.out.print("HellO_) world.");
         System.out.println("Welcome to Java");
     }
 }
